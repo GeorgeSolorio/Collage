0d9c71479ce175691d70d9fc814255c2c635ddad
GeorgeSolorio <Georgemsolorio@gmail.com>
2020-10-22 21:01:46 -0700

    Add isAuthorized publisher to notify subscribers about authorization status

--- Collage/Ultility/PHPhotoLibrary+Combine.swift
+++ Collage/Ultility/PHPhotoLibrary+Combine.swift
@@ -10,6 +10,15 @@ import Photos
 import Combine
 
 extension PHPhotoLibrary {
+   
+   static var isAuthorized: Future<Bool, Never> {
+     return Future { resolve in
+       self.fetchAuthorizationStatus { status in
+         resolve(.success(status))
+       }
+     }
+   }
+   
    static func fetchAuthorizationStatus(callback: @escaping (Bool) -> Void) {
       // Fetch the current status.
       let currentlyAuthorized = authorizationStatus() == .authorized
